33df210bc6045f98a0cbb492152ef0a76ad2307a
Alican Keçici <alican.kecici@gmail.com>
2023-07-10 21:27:01 +0300

    added

---
+++ src/org/csystem/homework/solution/homework_16/App.java
@@ -0,0 +1,17 @@
+package org.csystem.homework.solution.homework_16;
+
+public class App {
+    public static void main(String [] args)
+    {
+        Application.run();
+    }
+}
+class Application {
+    public static void run()
+    {
+        var a = new ArrayListExam();
+
+        a.run();
+    }
+}
+
---
+++ src/org/csystem/homework/solution/homework_16/ArrayListExam.java
@@ -0,0 +1,48 @@
+package org.csystem.homework.solution.homework_16;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayListExam {
+    private static final Scanner kb = new Scanner(System.in);
+    private final ArrayList<ArrayList<String>> ms_arrayList;
+    public ArrayListExam()
+    {
+        ms_arrayList = new ArrayList<>();
+    }
+
+    private void fillArrayList(String[] str, ArrayList<String> arrayList)
+    {
+        arrayList.addAll(Arrays.asList(str));
+        ms_arrayList.add(arrayList);
+    }
+    private void printArrayList()
+    {
+        System.out.println("<Names>");
+        for (var arr : ms_arrayList) {
+            System.out.print("\t<Line>\n");
+            for (String str : arr)
+                System.out.printf("\t\t<Name>%s</Name>\n", str);
+            System.out.print("\t</Line>\n");
+        }
+        System.out.println("</Names>");
+    }
+    private String[] splitLine(String str)
+    {
+        return str.split("[ ]+");
+    }
+    public void run()
+    {
+        while (true) {
+            System.out.print("Input the names in a line: ");
+            String s = kb.nextLine();
+
+            if (s.equalsIgnoreCase("exit"))
+                break;
+
+            fillArrayList(splitLine(s), new ArrayList<>());
+        }
+        printArrayList();
+    }
+}
